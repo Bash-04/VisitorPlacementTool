ca0d050b0935d38d5f61b11195d1266472a17f32
Bas Bakens <bas.bakens@live.nl>
2023-05-16 11:31:44 +0200

    Update VPT.cs

--- VisitorPlacementTool/VisitorPlacementToolLibrary/VPT.cs
+++ VisitorPlacementTool/VisitorPlacementToolLibrary/VPT.cs
@@ -2,6 +2,21 @@
 {
     public class VPT
     {
+        // Properties
         public List<Event> Events { get; private set; }
+
+        // Constructors
+        public VPT() 
+        {
+            Events = new List<Event>();
+        }
+
+        // Methods
+        public bool TryCreateNewEvent()
+        {
+            bool eventIsCreated = false;
+
+            return eventIsCreated;
+        }
     }
 }
